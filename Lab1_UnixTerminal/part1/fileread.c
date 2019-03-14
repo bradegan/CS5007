6b7d8ff7cf9378f41647c1f4304f5d71a476c8c7
eganb <46579244+eganb@users.noreply.github.com>
2019-03-14 12:06:46 -0400

    Update fileread.c

--- Lab1_UnixTerminal/part1/fileread.c
+++ Lab1_UnixTerminal/part1/fileread.c
@@ -2,3 +2,27 @@
 // Run with: ./fileread
 
 // Fill in the code!
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* f;
+
+    f = fopen("./data.txt", "r");
+
+    if (f == NULL) {
+        printf("fopen failed");
+        exit(1);
+    }
+
+    char buffer[255];
+
+    while (fscanf(f, "%s", buffer) != EOF) {
+        printf("Red Sox wins are: %s\n",buffer);
+    }
+
+    fclose(f);
+
+    return 0;
+}
