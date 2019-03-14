3adc559a7d34d5f296f24fad591835d0bbb815db
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-14 12:33:42 -0400

    hello world?

--- Lab1_UnixTerminal/part2/hello.s
+++ Lab1_UnixTerminal/part2/hello.s
@@ -1,1 +1,13 @@
-# Your code here
+.Ltext0:
+.LC0:
+        .string "Hello, World!"
+main:
+.LFB0:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        movl    $.LC0, %edi
+        movl    $0, %eax
+        call    printf
+        movl    $0, %eax
+        popq    %rbp
+        ret
