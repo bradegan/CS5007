43decfb55a8dc8ab7a393139b97cea23176d8193
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-14 11:58:44 -0400

    push

---
+++ Lab1_UnixTerminal/part1/swap_int.c
@@ -0,0 +1,21 @@
+#include <stdhio.h>
+#include <stdlib.h>
+
+void swap_int(int* integer1, int* integer2){
+    int temp = integer1;
+	
+
+
+
+}
+
+
+void main(){
+
+	x=5;
+	y=10;
+
+	swap_int(x, y);
+	println(x);
+
+}
