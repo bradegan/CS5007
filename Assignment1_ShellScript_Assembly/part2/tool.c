dfaba4a5fa7b66b8eef4d4d58ef23a8274716d4b
eganb <46579244+eganb@users.noreply.github.com>
2019-03-15 17:16:40 -0400

    Update tool.c

--- Assignment1_ShellScript_Assembly/part2/tool.c
+++ Assignment1_ShellScript_Assembly/part2/tool.c
@@ -1,1 +1,29 @@
-// Implement your cycle count tool here.
+#include <stdio.h>
+#include <string.h>
+
+int main(int argsc, char** argv) {
+    FILE *myFile;
+    myFile = fopen(argv[1], "r");
+    int max = 30;
+    int mov =0;
+    int num_instruction=0;
+    int num_cycles=0;
+    char* addset[] = {"add", "addq", "addb", "addl"};
+    while (!(feof(myFile))) {
+        char buffer[max];
+        fgets(buffer, max, myFile);
+
+        if (strstr(buffer, "mov")){
+            mov++;
+            num_instruction++;
+            num_cycles += 1;
+            printf("MOV%d",mov);
+        }
+        //
+
+
+        // line <- readline(t)
+        // strtok read, lowercase,
+        // parse_Line(line)
+    }
+}
