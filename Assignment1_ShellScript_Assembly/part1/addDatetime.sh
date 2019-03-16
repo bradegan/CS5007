85719afeccb22052fdebf54ba632171781b3a9fc
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-15 23:21:51 -0400

    updates

--- Assignment1_ShellScript_Assembly/part1/addDatetime.sh
+++ Assignment1_ShellScript_Assembly/part1/addDatetime.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# The line above makes your script executable.
-# You should write your tutorial in here.
-# Include comments inline with your script explaining what is going on.
 
-# You might start out by first saying the purpose of the script and
-# demonstrating its usage.
 
 # Can be shared with proper attribution to owner.
 
