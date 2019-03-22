419daa2ef538b120425b7ad1f878744406b3d214
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-22 15:19:12 -0400

    msg

--- Assignment2_CompilerTools/trace/main.c
+++ Assignment2_CompilerTools/trace/main.c
@@ -5,13 +5,18 @@ void msg(){
 	printf("Hello!\n");
 }
 
-
+// Uses trace.c file to calculate time ellapsed.
+// Count number of time a function is exectued using trace.c
 int main(){
 
+	extern int count;
+	extern double time_ellapsed;
+
 	int i;
-	for(i= 0;i  < 10; ++i){
+	for(i= 0;i  < 10000; ++i){
 		msg();
 	}
-
+	printf("Count is... %d\n", count);
+	printf("Time ellapsed %f\n", time_ellapsed);
 	return 0;
 }
--- Assignment2_CompilerTools/trace/trace.c
+++ Assignment2_CompilerTools/trace/trace.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
+#include<time.h>
 
+int count = 0;
+clock_t start_time;
+clock_t stop_time;
+double time_ellapsed = 0;
+
+
+// Function that is executed before any function is exectured.
+// Keeps track of count and time ellapsed by program.
 __attribute__((no_instrument_function))
 void __cyg_profile_func_enter(void *this_fn, void *call_site){
 
+	count ++;
+	
 	printf("Function entered\n");
+	start_time = clock();
+    
+
 
 }
-
+// Function that is executed every time after a method is executed.
+// Calcs ellapsed time.
 __attribute__((no_instrument_function))
 void __cyg_profile_func_exit(void *this_fn, void *call_site){
-
-	printf("Function exited\n");
+	printf("Function exited \n");
+	stop_time = (double) clock();
+	time_ellapsed += ((double) (stop_time - start_time) / CLOCKS_PER_SEC);
 
 }
+
+
+
+
 
