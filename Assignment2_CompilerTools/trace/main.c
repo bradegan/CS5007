d1e5cf26e9794e4b6ab7ae58bb732a29209d7e07
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-22 19:06:47 -0400

    update

--- Assignment2_CompilerTools/trace/main.c
+++ Assignment2_CompilerTools/trace/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-
+#include <unistd.h>
+extern int count;
+extern double time_ellapsed;
 
 void msg(){
 	printf("Hello!\n");
+	sleep(1);
 }
 
 // Uses trace.c file to calculate time ellapsed.
 // Count number of time a function is exectued using trace.c
 int main(){
 
-	extern int count;
-	extern double time_ellapsed;
+	
 
 	int i;
-	for(i= 0;i  < 10000; ++i){
+	for(i= 0;i  < 10; ++i){
 		msg();
 	}
 	printf("Count is... %d\n", count);
