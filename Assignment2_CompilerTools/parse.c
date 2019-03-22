1f98fe317164d294812c21fe151f04e2bd64a7c5
eganb <46579244+eganb@users.noreply.github.com>
2019-03-22 13:58:59 -0400

    Update parse.c

--- Assignment2_CompilerTools/parse.c
+++ Assignment2_CompilerTools/parse.c
@@ -1,1 +1,16 @@
 // Implement a working parser in this file that splits text into individual tokens.
+
+#include <stdio.h>
+#include <string.h>
+
+int main (int argsc, char **argv)
+{   char* pch;
+    char* str = argv[1];
+    pch = strtok (str," ,.-");
+    while (pch != NULL)
+    {
+        printf ("%s\n",pch);
+        pch = strtok(NULL, " ,.-");
+    }
+    return 0;
+}
