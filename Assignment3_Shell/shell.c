b1555ea9e1b4fc82bbd313a582294b56657d8efc
Bradford Egan <bradegan@login-students.ccs.neu.edu>
2019-03-24 15:26:09 -0400

    msg

--- Assignment3_Shell/shell.c
+++ Assignment3_Shell/shell.c
@@ -22,3 +22,17 @@ int main(){
 
 	return 0;
 }
+
+int main (int argsc, char **argv)
+{   
+    if ((argv[1]) != NULL){
+    	char* pch;
+    	char* str = argv[1];
+    	pch = strtok (str," ,.-");
+    	while (pch != NULL)
+    	{
+        	printf ("%s\n",pch);
+        	pch = strtok(NULL, " ,.-");
+    	}
+    return 0;}
+}
